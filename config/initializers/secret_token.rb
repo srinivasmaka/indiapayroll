7e62ae3f5a57e5b1ce52d00620685f014904f2dc
Syam <syamkumar.gadde@icentris.com>
2013-04-09 11:58:59 +0530

    initial app uploading

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Indiapayroll::Application.config.secret_token = 'f2d782a9a95a498c452a086617acd59657a583072cbe7104e31c64d78c2bacf3a714941073c6935ddd25405b6c8bbd26c409cab0beef262aa679f32bd35dc1bb'
