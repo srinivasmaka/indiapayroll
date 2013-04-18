de7752a3b4f335726b0d8146fbbb41d3bf2ee67c
srinivasarao.maka <icentrisgit@gmail.com>
2013-04-18 14:07:05 +0530

    vallidations and alignment config and slabs forms

--- app/models/config_table.rb
+++ app/models/config_table.rb
@@ -1,3 +1,4 @@
 class ConfigTable < ActiveRecord::Base
   attr_accessible :basic_percent, :conveyance, :edu_cess, :hra_percent, :professional_tax,:year
+  validates :basic_percent, :conveyance, :edu_cess, :hra_percent, :professional_tax,:year,:presence=> true   
 end
--- app/models/tax_slab.rb
+++ app/models/tax_slab.rb
@@ -1,3 +1,4 @@
 class TaxSlab < ActiveRecord::Base
   attr_accessible :deduction_percent, :min_tax, :slab_from, :slab_to, :year
+  validates :deduction_percent, :min_tax, :slab_from, :slab_to, :year,:presence=> true  
 end
