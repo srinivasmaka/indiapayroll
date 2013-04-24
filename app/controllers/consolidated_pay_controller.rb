c49e3213db682ea76b4aec2151fa53853b9547cb
rajyalakshmi <rajyalakshmi@rajyalakshmi.(none)>
2013-04-24 16:08:54 +0530

    show the correct values when there are records in payment history and bonus tables

--- app/controllers/consolidated_pay_controller.rb
+++ app/controllers/consolidated_pay_controller.rb
@@ -10,10 +10,11 @@ class ConsolidatedPayController < ApplicationController
 	for e in @employees do 
 		@emp_id= e.emp_id
 		@gross_pay= e.grossCTC
+		@gross_monthly = @gross_pay.to_f/12
 		@cp= ConsolidatedPayments.new
 
 		if PaymentHistory.find(:all, @emp_id).empty?
-			@gross_monthly = @gross_pay.to_f/12
+			
 			@cp.jan =@gross_monthly
 			@cp.feb= @gross_monthly
 			@cp.mar = @gross_monthly
@@ -28,28 +29,35 @@ class ConsolidatedPayController < ApplicationController
 			@cp.dec = @gross_monthly
 							
 		else
-			@ph =PaymentHistory.where(":emp_id= ? and payment_type!=? ", @emp_id,"Q"). order(:period_id)
-			@cp.apr=@ph[1]
+			@ph =PaymentHistory.where("emp_id= ? and period_type=? ", @emp_id,"M"). order(:period_id)
+			for i in 0..@ph.length-1 do 
+				@cp.instance_variable_set(@cp.instance_variables[i], @ph[i].gross_monthly)
+			end
+			for i in @ph.length..11 do 
+				@cp.instance_variable_set(@cp.instance_variables[i], @gross_monthly)
+			end
 			
 		end
 
 		if Bonus.find(:all, @emp_id).empty?
-			@cp.ps1 =0
-			@cp.ps2= 0
+			@cp.ps1 = 0
+			@cp.ps2 = 0
 			@cp.ps3 = 0
 			@cp.ps4 = 0
 
 		else
-			@bonusPh= PaymentHistory.where(":emp_id= ? and payment_type=? ", @emp_id, "Q")
-			@cp.ps1="0"
-			@cp.ps2="0"
-			@cp.ps3="0"
-			@cp.ps4="0"
+			@bonusPh= PaymentHistory.where("emp_id= ? and period_type=? ", @emp_id, "Q").order(:period_id)
+			for i in 12..@bonusPh.length-1 do 
+				@cp.instance_variable_set(@cp.instance_variables[i], @ph[i].gross_monthly)
+			end
+			for i in @bonusPh.length..15 do 
+				@cp.instance_variable_set(@cp.instance_variables[i], 0)
+			end
+			
 		end
-		 
-		@cp.total= @cp.jan + @cp.feb + @cp.mar + @cp.apr + @cp.may + @cp.jun + @cp.jul + @cp.aug + @cp.sep +
-					 @cp.oct + @cp.nov + @cp.dec 
 
+		@cp.total= @cp.jan + @cp.feb + @cp.mar + @cp.apr + @cp.may + @cp.jun + @cp.jul + @cp.aug + @cp.sep +
+					 @cp.oct + @cp.nov + @cp.dec + @cp.ps1 + @cp.ps2 + @cp.ps3 + @cp.ps4
 
 
 		@paymentsHash[@emp_id]= @cp
--- app/models/consolidated_payments.rb
+++ app/models/consolidated_payments.rb
@@ -5,6 +5,24 @@ class ConsolidatedPayments
 	
 	attr_accessor :jan, :feb, :mar, :apr, :may, :jun, :jul, :aug, :sep, :oct, :nov, :dec, :ps1, :ps2, :ps3, :ps4, :total
 
-	
+	def initialize 
+		@apr = 0
+		@may = 0
+		@jun = 0
+		@jul = 0
+		@aug = 0
+		@sep = 0
+		@oct = 0
+		@nov = 0
+		@dec = 0
+		@jan = 0
+		@feb = 0
+		@mar = 0
+		@ps1 = 0
+		@ps2 = 0
+		@ps3 = 0
+		@ps4 = 0
+		@total = 0
+	end
 
 end
