81d54044aca7aca38eaeb5dc3d1a4291a9980fec
srinivas.maka <icentrisgit@gmail.com>
2013-05-02 16:07:47 +0530

    showing period time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,13 +2,11 @@ module ApplicationHelper
   
   def current_period_info
     d = Date.today
-    @prev_month_start_date  = d.prev_month().at_beginning_of_month()
-    @prev_month_end_date = prev_month().at_end_of_month()
-    
-    @pay_period = PayPeriod.where("start_date" => @prev_month_start_date , "end_date" => @prev_month_end_date).first
-    @period_id = @pay_period.period_id
-    @current_fyear = @pay_period.current_fyear
-    puts "period_id  #{@period_id} , current fyear  #{@current_fyear}"
+    pay_details=Hash.new
+    pay_details[:prev_month_start_date]  = d.prev_month().at_beginning_of_month()
+    pay_details[:prev_month_end_date] = d.prev_month().at_end_of_month()
+    pay_details[:pay_period] = PayPeriod.where("start_date" => @prev_month_start_date , "end_date" => @prev_month_end_date).first
+    pay_details
   end
   
 end
